44a80fc424b15a6eaa7404d8dab0ec2819dcc0f2
DiegoVelasco84 <83100265+DiegoVelasco84@users.noreply.github.com>
2023-09-21 15:25:39 -0500

    Ajustes 2

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -2,7 +2,7 @@
 .curso-main-container.pb-3
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    p.mb-5 A continuación, se muestra un mapa conceptual con los elementos más importantes desarrollados en este componente.
+    p.mb-5 La promoción y seguimiento de productos farmacéuticos es crucial para informar sobre medicamentos, generar confianza y aumentar su demanda. Se realiza a través de estrategias como visitas a profesionales de la salud, participación en eventos médicos y publicidad. El seguimiento implica monitorear la eficacia y seguridad de los medicamentos en el mercado, recopilando información y realizando estudios de farmacovigilancia. Ambas actividades buscan garantizar el uso seguro y efectivo de los productos farmacéuticos, cumpliendo con regulaciones y manteniendo una comunicación abierta con profesionales y pacientes.
 
     .row.justify-content-center
       .col-lg-10.mb-5
